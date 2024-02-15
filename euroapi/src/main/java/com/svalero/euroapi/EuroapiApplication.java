e8eb36003571404cafed2224415ce0ba75441859
Mapi <mapitenar@hotmail.com>
2024-02-15 16:00:52 +0100

    Initialize

---
+++ euroapi/src/main/java/com/svalero/euroapi/EuroapiApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.euroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroapiApplication.class, args);
+	}
+
+}
---
+++ euroapi/src/test/java/com/svalero/euroapi/EuroapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.euroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
